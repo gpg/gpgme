75445138179272158bca3d673466c4c09ed1ae49
Werner Koch <wk@gnupg.org>
2001-01-18 09:11:49 +0000

    Does now compile under MingW32/CPD 0.3

--- trunk/gpgme/gpgme.h
+++ trunk/gpgme/gpgme.h
@@ -42,7 +42,7 @@ extern "C" {
  * let autoconf (using the AM_PATH_GPGME macro) check that this
  * header matches the installed library.
  * Warning: Do not edit the next line.  configure will do that for you! */
-#define GPGME_VERSION "0.1.4"
+#define GPGME_VERSION "0.1.4a"
 
 
 
