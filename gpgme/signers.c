c3e7053489df648dfccf9bb4146c374aa229b483
Marcus Brinkmann <mb@g10code.com>
2001-10-29 00:08:14 +0000

    gpgme/ 2001-10-29  Marcus Brinkmann  <marcus@g10code.de>
    	* context.h: New member signers_len.  * signers.c
    	(gpgme_signers_clear): Require that signers are non-NULL with
    	assertion.  Use signers_len to determine how much keys to release.
    	Add documentation.
    	(gpgme_signers_add): Use signers_len to determine if the buffer is
    	large enough.  Use xtryrealloc rather than xtrymalloc and copying.
    	Add documentation.
    	(gpgme_signers_enum): Use signers_len to determine if key is
    	available.  Add documentation.
    
    tests/
    2001-10-29  Marcus Brinkmann  <marcus@g10code.de>
    
    	* t-signers.c: New file.
    	* Makefile.am (TESTS): Add t-signers.

--- gpgme/signers.c
+++ gpgme/signers.c
@@ -33,76 +33,88 @@
  */
 
 
+/**
+ * gpgme_signers_clear:
+ * @c: context to clear from signers
+ *
+ * Remove the list of signers from the context and release the
+ * references to the signers keys.
+ *
+ * Return value: The version string or NULL
+ **/
 void
 gpgme_signers_clear (GpgmeCtx c)
 {
-    int i;
+  int i;
 
-    return_if_fail (c);
+  return_if_fail (c);
 
-    if (!c->signers)
-        return;
-    for (i=0; i < c->signers_size; i++ ) {
-        if (!c->signers[i])
-            break;
-        gpgme_key_unref (c->signers[i]);
-        c->signers[i] = NULL;
+  if (!c->signers)
+    return;
+  for (i = 0; i < c->signers_len; i++)
+    {
+      assert (c->signers[i]);
+      gpgme_key_unref (c->signers[i]);
+      c->signers[i] = NULL;
     }
+  c->signers_len = 0;
 }
 
-
+/**
+ * gpgme_signers_add:
+ * @c: context to add signer to
+ * @key: key to add
+ *
+ * Add the key as a signer to the context.  Acquires a reference to
+ * the key.
+ *
+ * Return value: NULL on success, or an error code.
+ **/
 GpgmeError
 gpgme_signers_add (GpgmeCtx c, const GpgmeKey key)
 {
-    int i = 0;
+  if (!c || !key)
+    return mk_error (Invalid_Value);
 
-    if (!c || !key)
-        return mk_error (Invalid_Value);
+  if (c->signers_len == c->signers_size)
+    {
+      GpgmeKey *newarr;
+      int n = c->signers_size + 5;
+      int j;
 
-    if (!c->signers)
-        c->signers_size = 0;
-
-    for (i=0; i < c->signers_size && c->signers[i]; i++ )
-        ;
-    if ( !(i < c->signers_size) ) {
-        GpgmeKey *newarr;
-        int j;
-        int n = c->signers_size + 5;
-
-        newarr = xtrycalloc ( n, sizeof *newarr );
-        if ( !newarr )
-            return mk_error (Out_Of_Core);
-        for (j=0; j < c->signers_size; j++ )
-            newarr[j] = c->signers[j];
-        c->signers_size = n;
-        xfree (c->signers);
-        c->signers = newarr;
+      newarr = xtryrealloc (c->signers, n * sizeof (*newarr));
+      if (!newarr)
+	return mk_error (Out_Of_Core);
+      for (j = c->signers_size; j < n; j++)
+	newarr[j] = NULL;
+      c->signers = newarr;
+      c->signers_size = n;
     }
-    gpgme_key_ref (key);
-    c->signers[i] = key;
-    return 0;
+
+  gpgme_key_ref (key);
+  c->signers[c->signers_len++] = key;
+  return 0;
 }
 
-
+/**
+ * gpgme_signers_enum:
+ * @c: context to retrieve signer from
+ * @seq: index of key to retrieve
+ *
+ * Acquire a reference to the signers key with the specified index
+ * number in the context and return it to the caller.
+ *
+ * Return value: A GpgmeKey or NULL on failure.
+ **/
 GpgmeKey
 gpgme_signers_enum (const GpgmeCtx c, int seq )
 {
-    int i;
+  return_null_if_fail (c);
+  return_null_if_fail (seq >= 0);
 
-    return_null_if_fail (c);
-    return_null_if_fail (seq>=0);
-
-    if (!c->signers)
-        c->signers_size = 0;
-    for (i=0; i < c->signers_size && c->signers[i]; i++ ) {
-        if (i==seq) {
-            gpgme_key_ref (c->signers[i]);
-            return c->signers[i];
-        }
-    }
+  if (seq >= c->signers_len)
     return NULL;
+
+  gpgme_key_ref (c->signers[seq]);
+  return c->signers[seq];
 }
-
-
-
-
