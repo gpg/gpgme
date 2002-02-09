55cd54f02b0781cdf74485abf7a107ce7720d229
Marcus Brinkmann <mb@g10code.com>
2002-02-09 01:04:12 +0000

    Update as edited by configure.

--- gpgme/gpgme.h
+++ gpgme/gpgme.h
@@ -43,7 +43,7 @@ extern "C" {
    AM_PATH_GPGME macro) check that this header matches the installed
    library.  Warning: Do not edit the next line.  configure will do
    that for you!  */
-#define GPGME_VERSION "0.3.0a-cvs"
+#define GPGME_VERSION "0.3.1"
 
 
 /* The opaque data types used by GPGME.  */
