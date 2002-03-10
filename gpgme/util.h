d901ebf3b91a81043f6a68ff5e43b82ea3b50f29
Werner Koch <wk@gnupg.org>
2002-03-10 18:35:33 +0000

    Minor doc fixes

--- gpgme/util.h
+++ gpgme/util.h
@@ -139,7 +139,7 @@ typedef struct
   ssize_t (*read)(void*,char*,size_t);
   ssize_t (*write)(void*,const char*,size_t);
   int (*seek)(void*,off_t*,int);
-  int (*close)(coid*);
+  int (*close)(void*);
 } _IO_cookie_io_functions_t;
 typedef _IO_cookie_io_functions_t cookie_io_functions_t;
 FILE *fopencookie (void *cookie, const char *opentype,
