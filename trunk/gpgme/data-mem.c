d8426127c27d17c840310b490ae6046d3e9cd177
Marcus Brinkmann <mb@g10code.com>
2002-12-06 22:06:25 +0000

    doc/ 2002-12-06  Marcus Brinkmann  <marcus@g10code.de>
    	* gpgme.texi (Memory Based Data Buffers): New subsection.
    	(File Based Data Buffers): Likewise.
    	(Callback Based Data Buffers): Likewise.
    	(Manipulating Data Buffers): Update interfaces.  Add
    	gpgme_data_seek.
    	* gpgme.texi (Engine Version Check): Remove gpgme_chec_engine.
    
    gpgme/
    2002-12-06  Marcus Brinkmann  <marcus@g10code.de>
    
    	* data.h (gpgme_data_release_cb): Change return type to void.
    	(gpgme_data_read_cb): Change return type to ssize_t.
    	* data.c (gpgme_data_read): Likewise.
    	* data-stream.c (stream_read): Likewise.
    	* data-fd.c (fd_read): Likewise.
    	* data-mem.c (mem_read): Likewise.
    	(mem_release): Change return type to void.
    	* data-user.c (user_read): Change return type to ssize_t.
    	(user_release): Change return type to void.
    	* data-compat.c (old_user_read): Change return type to ssize_t.
    	* gpgme.h (GpgmeDataReadCb): Likewise.
    	(gpgme_data_read): Likewise.
    	(GpgmeDataSeekCb): Change return type to off_t.

--- trunk/gpgme/data-mem.c
+++ trunk/gpgme/data-mem.c
@@ -1,22 +1,21 @@
 /* data-mem.c - A memory based data object.
- *      Copyright (C) 2002 g10 Code GmbH
- *
- * This file is part of GPGME.
- *
- * GPGME is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * GPGME is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
- */
+   Copyright (C) 2002 g10 Code GmbH
+ 
+   This file is part of GPGME.
+ 
+   GPGME is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+ 
+   GPGME is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public License
+   along with GPGME; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #if HAVE_CONFIG_H
 #include <config.h>
@@ -31,7 +30,7 @@
 #include "util.h"
 
 
-static int
+static ssize_t
 mem_read (GpgmeData dh, void *buffer, size_t size)
 {
   size_t amt = dh->data.mem.length - dh->data.mem.offset;
@@ -141,7 +140,7 @@ mem_seek (GpgmeData dh, off_t offset, int whence)
 }
 
 
-static int
+static void
 mem_release (GpgmeData dh)
 {
   if (dh->data.mem.buffer)
