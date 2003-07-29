95390e074ac3c68d1b477170b662ed13d5b6b1ac
Marcus Brinkmann <mb@g10code.com>
2003-07-29 19:34:03 +0000

    2003-07-29  Marcus Brinkmann  <marcus@g10code.de>
    	* decrypt-verify.c (decrypt_verify_status_handler): Expand silly
    	and wrong expression.
    	* encrypt-sign.c (encrypt_sign_status_handler): Likewise.
    	* encrypt.c (encrypt_sym_status_handler): Likewise.
    	* sign.c (sign_status_handler): Likewise.
    	* verify.c (verify_status_handler): Likewise.
    	* decrypt.c (decrypt_status_handler): Likewise.

--- gpgme/decrypt.c
+++ gpgme/decrypt.c
@@ -147,8 +147,12 @@ _gpgme_decrypt_status_handler (void *priv, gpgme_status_code_t code,
 static gpgme_error_t
 decrypt_status_handler (void *priv, gpgme_status_code_t code, char *args)
 {
-  return _gpgme_progress_status_handler (priv, code, args)
-    || _gpgme_decrypt_status_handler (priv, code, args);
+  gpgme_error_t err;
+
+  err = _gpgme_progress_status_handler (priv, code, args);
+  if (!err)
+    err = _gpgme_decrypt_status_handler (priv, code, args);
+  return err;
 }
 
 
