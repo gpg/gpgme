d077b5477f973cbe6fef771d5e54274bca794e55
Werner Koch <wk@gnupg.org>
2003-11-19 08:53:47 +0000

    Post release version number bump

--- branches/gpgme-0-3-branch/gpgme/gpgme.h
+++ branches/gpgme-0-3-branch/gpgme/gpgme.h
@@ -44,7 +44,7 @@ extern "C" {
    AM_PATH_GPGME macro) check that this header matches the installed
    library.  Warning: Do not edit the next line.  configure will do
    that for you!  */
-#define GPGME_VERSION "0.3.16"
+#define GPGME_VERSION "0.3.17-cvs"
 
 
 /* The opaque data types used by GPGME.  */
