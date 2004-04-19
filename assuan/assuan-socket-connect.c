7eb7b24228128dd6dfc8a822b1ff3e0bfa820f80
Werner Koch <wk@gnupg.org>
2004-04-19 14:24:57 +0000

    * assuan-socket-connect.c: Include sys/types.h * assuan-socket-server.c: Ditto * assuan-domain-connect.c: Ditto.

--- assuan/assuan-socket-connect.c
+++ assuan/assuan-socket-connect.c
@@ -23,6 +23,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
--- assuan/assuan-socket-server.c
+++ assuan/assuan-socket-server.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
