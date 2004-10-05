9bccf0d83016d20911c483a2d1f59095fbf83331
Marcus Brinkmann <mb@g10code.com>
2004-10-05 15:08:45 +0000

    2004-10-05  Marcus Brinkmann  <marcus@g10code.de>
    	* op-support.c (_gpgme_op_data_lookup): Use char pointer for
    	pointer arithmetic.

--- gpgme/op-support.c
+++ gpgme/op-support.c
@@ -50,7 +50,7 @@ _gpgme_op_data_lookup (gpgme_ctx_t ctx, ctx_op_data_id_t type, void **hook,
       data->next = ctx->op_data;
       data->type = type;
       data->cleanup = cleanup;
-      data->hook = ((void *) data) + sizeof (struct ctx_op_data);
+      data->hook = (void *) (((char *) data) + sizeof (struct ctx_op_data));
       ctx->op_data = data;
     }
   *hook = data->hook;
