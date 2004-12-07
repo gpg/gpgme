71775ad8fcdcc5e7066b94fa29dd7c44c3469c38
Marcus Brinkmann <mb@g10code.com>
2004-12-07 21:13:39 +0000

    2004-12-07  Marcus Brinkmann  <marcus@g10code.de>
    	* README: Refer to COPYING.LESSER and "each file" instead of
    	COPYING.
    	* COPYING.LESSER: New file.
    	* gpgme.spec.in (%doc): Add COPYING.LESSER.
    	* acinclude.m4, configure.ac, Makefile.am: Change license to LGPL
    	2.1 or later.
    	* TODO: Add copyright notice.
    	* README.CVS: Likewise.
    
    assuan/
    2004-12-07  Marcus Brinkmann  <marcus@g10code.de>
    
    	* README.1st: Add copyright notice.
    
    doc/
    2004-12-07  Marcus Brinkmann  <marcus@g10code.de>
    
    	* Makefile.am: Change license to LGPL.
    	(gpgme_TEXINFOS): Replace gpl.texi with lesser.texi.
    
    	* gpgme.texi: Change license to LGPL (also for documentation of
    	GPGME's license).
    	* lesser.texi: New file.
    	* gpl.texi: File removed.
    
    gpgme/
    2004-12-07  Marcus Brinkmann  <marcus@g10code.de>
    
    	* putc_unlocked.c, funopen.c: I just claim copyright on these
    	files and change their license to LGPL, because they are totally
    	trivial wrapper functions.
    	* isascii.c: Change copyright notice to the one from ctype/ctype.h
    	in the GNU C Library (CVS Head 2004-10-10), where isascii is
    	defined as a macro doing exactly the same as the function in this
    	file.
    	* memrchr.c: Update from the GNU C Library (CVS Head 2001-07-06).
    	* stpcpy.c: Update from the GNU C Library (CVS Head 2004-10-10).
    	* ath.c, ath-compat.c, ath.h, ath-pth.c, ath-pth-compat.c,
    	ath-pthread.c, ath-pthread-compat.c, context.h, conversion.c,
    	data.c, data-compat.c, data-fd.c, data.h, data-mem.c,
    	data-stream.c, data-user.c, debug.c, debug.h, decrypt.c,
    	decrypt-verify.c, delete.c, edit.c, encrypt.c, encrypt-sign.c,
    	engine-backend.h, engine.c, engine-gpgsm.c, engine.h, error.c,
    	export.c, genkey.c, get-env.c, gpgme.c, gpgme.h, import.c, io.h,
    	key.c, keylist.c, mkstatus, Makefile.am, ops.h, op-support.c,
    	passphrase.c, posix-io.c, posix-sema.c, posix-util.c, progress.c,
    	rungpg.c, sema.h, sign.c, signers.c, trust-item.c, trustlist.c,
    	util.h, verify.c, version.c, w32-io.c, w32-sema.c, w32-util.c,
    	wait.c, wait-global.c, wait.h, wait-private.c, wait-user.c: Change
    	license to LGPL.
    
    tests/
    2004-12-07  Marcus Brinkmann  <marcus@g10code.de>
    
    	* gpg/mkdemodirs: Add copyright notice.
    
    	* gpgsm/Makefile.am, gpgsm/t-support.h, gpgsm/t-decrypt.c,
    	gpgsm/t-encrypt.c, gpgsm/t-export.c, gpgsm/t-genkey.c,
    	gpgsm/t-import.c, gpgsm/t-keylist.c, gpgsm/t-sign.c,
    	gpgsm/t-verify.c, gpg/Makefile.am, gpg/t-decrypt.c,
    	gpg/t-decrypt-verify.c, gpg/t-edit.c, gpg/t-encrypt.c,
    	gpg/t-encrypt-sign.c, gpg/t-encrypt-sym.c, gpg/t-eventloop.c,
    	gpg/t-export.c, gpg/t-genkey.c, gpg/t-import.c, gpg/t-keylist.c,
    	gpg/t-keylist-sig.c, gpg/t-sign.c, gpg/t-signers.c,
    	gpg/t-support.h, gpg/t-thread1.c, gpg/t-trustlist.c,
    	gpg/t-verify.c, Makefile.am, t-data.c, t-engine-info.c,
    	t-version.c: Change license to LGPL.

--- tests/gpg/t-genkey.c
+++ tests/gpg/t-genkey.c
@@ -1,22 +1,23 @@
-/* t-genkey.c - regression test
+/* t-genkey.c - Regression test.
    Copyright (C) 2000 Werner Koch (dd9jn)
-   Copyright (C) 2001, 2003 g10 Code GmbH
+   Copyright (C) 2001, 2003, 2004 g10 Code GmbH
 
    This file is part of GPGME.
  
    GPGME is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
- 
+   under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+   
    GPGME is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
- 
-   You should have received a copy of the GNU General Public License
-   along with GPGME; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.  */
 
 /* We need to include config.h so that we know whether we are building
    with large file system (LFS) support. */
