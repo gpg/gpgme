7d0c7409ef90a79a405fdfb5cb27b91e17f501fd
Werner Koch <wk@gnupg.org>
2004-12-28 11:32:51 +0000

    * configure.ac: Require automake 1.9.3 and autoconf 2.59. * acinclude.m4: Changed quoting for automake 1.9. * README: Use SHA1 instead of MD5.
    * pth.m4: Changed quoting for use with automake 1.9.

--- gpgme/gpgme.h
+++ gpgme/gpgme.h
@@ -75,7 +75,7 @@ extern "C" {
    AM_PATH_GPGME macro) check that this header matches the installed
    library.  Warning: Do not edit the next line.  configure will do
    that for you!  */
-#define GPGME_VERSION "1.0.1"
+#define GPGME_VERSION "1.0.2"
 
 
 /* Some opaque data types used by GPGME.  */
