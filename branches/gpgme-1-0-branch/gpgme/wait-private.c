998a35905643069b4b69c181457430cac9487fed
Marcus Brinkmann <mb@g10code.com>
2005-06-02 22:05:32 +0000

    2005-06-02  Marcus Brinkmann  <marcus@g10code.de>
    	* wait.h (_gpgme_run_io_cb): New prototype.
    	* wait.c (_gpgme_run_io_cb): New function.
    	* wait-global.c (gpgme_wait): Call it.
    	* wait-user.c (_gpgme_user_io_cb_handler): Likewise.
    	* wait-private.c (_gpgme_wait_on_condition): Likewise.

--- branches/gpgme-1-0-branch/gpgme/wait-private.c
+++ branches/gpgme-1-0-branch/gpgme/wait-private.c
@@ -101,15 +101,11 @@ _gpgme_wait_on_condition (gpgme_ctx_t ctx, volatile int *cond)
 	{
 	  if (ctx->fdt.fds[i].fd != -1 && ctx->fdt.fds[i].signaled)
 	    {
-	      struct wait_item_s *item;
-	      
 	      ctx->fdt.fds[i].signaled = 0;
 	      assert (nr);
 	      nr--;
-	      
-	      item = (struct wait_item_s *) ctx->fdt.fds[i].opaque;
 
-	      err = item->handler (item->handler_value, ctx->fdt.fds[i].fd);
+	      err = _gpgme_run_io_cb (&ctx->fdt.fds[i], 0);
 	      if (err)
 		{
 		  /* An error occured.  Close all fds in this context,
