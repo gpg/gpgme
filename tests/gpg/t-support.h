8a1ce95287705641f20d75619190de2bb2099a06
Werner Koch <wk@gnupg.org>
2005-09-23 13:29:04 +0000

    Boosted performance of w32 I/O

--- tests/gpg/t-support.h
+++ tests/gpg/t-support.h
@@ -96,7 +96,9 @@ init_gpgme (gpgme_protocol_t proto)
   gpgme_check_version (NULL);
   setlocale (LC_ALL, "");
   gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
+#ifndef HAVE_W32_SYSTEM
   gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
+#endif
 
   err = gpgme_engine_check_version (proto);
   fail_if_err (err);
