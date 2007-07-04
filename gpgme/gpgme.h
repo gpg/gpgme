2d7cfe08b0654ba21a2455ace42eb316ef61725e
Marcus Brinkmann <mb@g10code.com>
2007-07-04 21:07:27 +0000

    2007-07-04  Marcus Brinkmann  <marcus@g10code.de>
    	* assuan/Makefile.am (INCLUDES): Include $(top_srcdir)/gpgme.  *
    	assuan/assuan.h: Include <ath.h> instead of trying to duplicate
    	the definitions.

--- gpgme/gpgme.h
+++ gpgme/gpgme.h
@@ -72,7 +72,7 @@ extern "C" {
    AM_PATH_GPGME macro) check that this header matches the installed
    library.  Warning: Do not edit the next line.  configure will do
    that for you!  */
-#define GPGME_VERSION "1.1.5-cvs1213"
+#define GPGME_VERSION "1.1.5-cvs1219"
 
 
 
