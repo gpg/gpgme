906da085cf6a15173b1ffdf698159d55fd0d67fe
Werner Koch <wk@gnupg.org>
2007-07-12 15:25:20 +0000

    Changes for W32

--- tests/gpgsm/t-support.h
+++ tests/gpgsm/t-support.h
@@ -91,9 +91,11 @@ init_gpgme (gpgme_protocol_t proto)
   gpgme_error_t err;
 
   gpgme_check_version (NULL);
+#ifndef HAVE_W32_SYSTEM
   setlocale (LC_ALL, "");
   gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
   gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
+#endif
 
   err = gpgme_engine_check_version (proto);
   fail_if_err (err);
