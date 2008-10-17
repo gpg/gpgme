cc3485b674a1da7d19238a1593c4fe41e167038a
Marcus Brinkmann <mb@g10code.com>
2008-10-17 23:10:26 +0000

    2008-10-18  Marcus Brinkmann  <marcus@g10code.com>
    	* w32-util.c (find_program_in_registry): Don't define.
    	(_gpgme_get_gpg_path, _gpgme_get_gpgsm_path)
    	(_gpgme_get_gpgconf_path): Do not check for fooProgram in the
    	registry anymore.  It is now no longer possible to overwrite the
    	default location in that way.

--- trunk/gpgme/w32-util.c
+++ trunk/gpgme/w32-util.c
@@ -225,6 +225,7 @@ w32_shgetfolderpath (HWND a, int b, HANDLE c, DWORD d, LPSTR e)
 }
 
 
+#if 0
 static char *
 find_program_in_registry (const char *name)
 {
@@ -245,6 +246,7 @@ find_program_in_registry (const char *name)
     }
   return program;
 }
+#endif
 
 
 static char *
@@ -307,8 +309,10 @@ _gpgme_get_gpg_path (void)
   static char *gpg_program;
 
   LOCK (get_path_lock);
+#if 0
   if (!gpg_program)
     gpg_program = find_program_in_registry ("gpgProgram");
+#endif
   if (!gpg_program)
     gpg_program = find_program_in_inst_dir ("gpg.exe");
   if (!gpg_program)
@@ -324,8 +328,10 @@ _gpgme_get_gpgsm_path (void)
   static char *gpgsm_program;
 
   LOCK (get_path_lock);
+#if 0
   if (!gpgsm_program)
     gpgsm_program = find_program_in_registry ("gpgsmProgram");
+#endif
   if (!gpgsm_program)
     gpgsm_program = find_program_in_inst_dir ("gpgsm.exe");
   if (!gpgsm_program)
@@ -341,8 +347,10 @@ _gpgme_get_gpgconf_path (void)
   static char *gpgconf_program;
 
   LOCK (get_path_lock);
+#if 0
   if (!gpgconf_program)
     gpgconf_program = find_program_in_registry ("gpgconfProgram");
+#endif
   if (!gpgconf_program)
     gpgconf_program = find_program_in_inst_dir ("gpgconf.exe");
   if (!gpgconf_program)
