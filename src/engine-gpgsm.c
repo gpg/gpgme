2c5d801fc4900cec5b7dc7faa496f8664ab104f4
Marcus Brinkmann <mb@g10code.com>
2009-05-18 17:38:31 +0000

    doc/ 2009-05-18  Marcus Brinkmann  <marcus@g10code.de>
            * gpgme.texi (Encrypting a Plaintext): Document                                             
            GPGME_ENCRYPT_NO_ENCRYPT_TO.                                                                
                                                                                                        
    src/                                                                                                
    2009-05-18  Marcus Brinkmann  <marcus@g10code.de>                                                   
                                                                                                        
            * gpgme.h.in (gpgme_encrypt_flags_t): Add                                                   
            GPGME_ENCRYPT_NO_ENCRYPT_TO.                                                                
            * engine-gpg.c (gpg_encrypt): Pass --no-encrypt-to to gpg if                                
            GPGME_ENCRYPT_NO_ENCRYPT_TO flag is set.

--- src/engine-gpgsm.c
+++ src/engine-gpgsm.c
@@ -1349,6 +1349,14 @@ gpgsm_encrypt (void *engine, gpgme_key_t recp[], gpgme_encrypt_flags_t flags,
   if (!recp)
     return gpg_error (GPG_ERR_NOT_IMPLEMENTED);
 
+  if (flags & GPGME_ENCRYPT_NO_ENCRYPT_TO)
+    {
+      err = gpgsm_assuan_simple_command (gpgsm->assuan_ctx,
+					 "OPTION no-encrypt-to", NULL, NULL);
+      if (err)
+	return err;
+    }
+
   gpgsm->input_cb.data = plain;
   err = gpgsm_set_fd (gpgsm, INPUT_FD, map_data_enc (gpgsm->input_cb.data));
   if (err)
