a4c4ee1aaea4359432bdf60dd572f549ef8dee83
Werner Koch <wk@gnupg.org>
2011-12-02 11:32:31 +0100

    Generate the ChangeLog from commit logs.
    * build-aux/gitlog-to-changelog: New script.  Taken from gnulib.
    * build-aux/git-log-fix: New file.
    * build-aux/git-log-footer: New file.
    * build-aux/git-hook/commit-msg: New script.
    * doc/HACKING: New file.
    * ChangeLog: New file.
    * Makefile.am (EXTRA_DIST): Add new files.
    (gen-ChangeLog): New.
    (dist-hook): Run gen-ChangeLog.
    * autogen.sh: Install commit-msg hook for git.
    
    Rename all ChangeLog files to ChangeLog-2011.

--- autogen.sh
+++ autogen.sh
@@ -243,11 +243,36 @@ if test "$DIE" = "yes"; then
     cat <<EOF
 
 Note that you may use alternative versions of the tools by setting
-the corresponding environment variables; see README.CVS for details.
+the corresponding environment variables; see README.GIT for details.
 
 EOF
     exit 1
 fi
+
+
+# Update the git setup.
+if [ -d .git ]; then
+  if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
+    cat <<EOF >&2
+*** Activating trailing whitespace git pre-commit hook. ***
+    For more information see this thread:
+      http://mail.gnome.org/archives/desktop-devel-list/2009-May/msg00084html
+    To deactivate this pre-commit hook again move .git/hooks/pre-commit
+    and .git/hooks/pre-commit.sample out of the way.
+EOF
+      cp -av .git/hooks/pre-commit.sample .git/hooks/pre-commit
+      chmod -c +x  .git/hooks/pre-commit
+  fi
+  if [ -f build-aux/git-hooks/commit-msg -a ! -f .git/hooks/commit-msg ] ; then
+    cat <<EOF >&2
+*** Activating commit log message check hook. ***
+EOF
+      cp -av build-aux/git-hooks/commit-msg .git/hooks/commit-msg
+      chmod -c +x  .git/hooks/commit-msg
+  fi
+fi
+
+
 
 echo "Running aclocal -I m4 ${ACLOCAL_FLAGS:+$ACLOCAL_FLAGS }..."
 $ACLOCAL -I m4 $ACLOCAL_FLAGS
