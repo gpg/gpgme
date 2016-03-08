8347f3d5fc3e476aa767fbbaf09a1310a6154280
Andre Heinecke <aheinecke@intevation.de>
2016-03-08 14:33:15 +0100

    Add qgpgme as qt language binding
    * configure.ac: Add version defines. Check for qt if neccessary.
    * lang/README: Mention qt
    * lang/cpp/src/GpgmeppConfig.cmake.in.in: Remove comment. Find qgpgme.
    * lang/qt/src/Makefile.am: New. Build qgpgme.
    * lang/qt/README,
     lang/qt/src/Makefile.am,
     lang/qt/src/QGpgmeConfig.cmake.in.in,
     lang/qt/src/QGpgmeConfigVersion.cmake.in,
     lang/qt/src/dataprovider.cpp,
     lang/qt/src/dataprovider.h,
     lang/qt/src/qgpgme_export.h,
     m4/qt.m4: New.
    * lang/cpp/src/GpgmeppConfig.cmake.in.in,
     lang/cpp/src/Makefile.am: Fix generated config file.
    
    --
    For now this is just the dataprovider which was part of the
    KF5 Gpgmepp QGpgme variant. This is very thin but a useful
    class which is used downstream.

---
+++ lang/qt/src/dataprovider.h
@@ -0,0 +1,104 @@
+/* dataprovider.h
+   Copyright (C) 2004 Klarälvdalens Datakonsult AB
+
+   This file is part of QGPGME.
+
+   QGPGME is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   QGPGME is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with QGPGME; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA. */
+
+// -*- c++ -*-
+#ifndef __QGPGME_DATAPROVIDER_H__
+#define __QGPGME_DATAPROVIDER_H__
+
+#include "qgpgme_export.h"
+#include <interfaces/dataprovider.h>
+
+#include <QtCore/QByteArray>
+
+#include <boost/shared_ptr.hpp>
+
+class QIODevice;
+
+namespace QGpgME
+{
+
+class QGPGME_EXPORT QByteArrayDataProvider : public GpgME::DataProvider
+{
+public:
+    QByteArrayDataProvider();
+    explicit QByteArrayDataProvider(const QByteArray &initialData);
+    ~QByteArrayDataProvider();
+
+    const QByteArray &data() const
+    {
+        return mArray;
+    }
+
+private:
+    // these shall only be accessed through the dataprovider
+    // interface, where they're public:
+    /*! \reimp */
+    bool isSupported(Operation) const
+    {
+        return true;
+    }
+    /*! \reimp */
+    ssize_t read(void *buffer, size_t bufSize);
+    /*! \reimp */
+    ssize_t write(const void *buffer, size_t bufSize);
+    /*! \reimp */
+    off_t seek(off_t offset, int whence);
+    /*! \reimp */
+    void release();
+
+private:
+    QByteArray mArray;
+    off_t mOff;
+};
+
+class QGPGME_EXPORT QIODeviceDataProvider : public GpgME::DataProvider
+{
+public:
+    explicit QIODeviceDataProvider(const boost::shared_ptr<QIODevice> &initialData);
+    ~QIODeviceDataProvider();
+
+    const boost::shared_ptr<QIODevice> &ioDevice() const
+    {
+        return mIO;
+    }
+
+private:
+    // these shall only be accessed through the dataprovider
+    // interface, where they're public:
+    /*! \reimp */
+    bool isSupported(Operation) const;
+    /*! \reimp */
+    ssize_t read(void *buffer, size_t bufSize);
+    /*! \reimp */
+    ssize_t write(const void *buffer, size_t bufSize);
+    /*! \reimp */
+    off_t seek(off_t offset, int whence);
+    /*! \reimp */
+    void release();
+
+private:
+    const boost::shared_ptr<QIODevice> mIO;
+    bool mErrorOccurred : 1;
+    bool mHaveQProcess  : 1;
+};
+
+} // namespace QGpgME
+
+#endif
