20b02a7dd49c7ede148245d8a5319b2ba9ce7673
Andre Heinecke <aheinecke@intevation.de>
2016-05-06 14:20:45 +0200

    Qt: Fix library name in Copyright headers
    --
    Files are not part of libkleopatra anymore.

--- lang/qt/src/abstractimportjob.h
+++ lang/qt/src/abstractimportjob.h
@@ -5,12 +5,12 @@
     Copyright (c) 2009 Klarälvdalens Datakonsult AB
     Copyright (c) 2016 Intevation GmbH
 
-    Libkleopatra is free software; you can redistribute it and/or
+    QGpgME is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
     published by the Free Software Foundation; either version 2 of the
     License, or (at your option) any later version.
 
-    Libkleopatra is distributed in the hope that it will be useful,
+    QGpgME is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     General Public License for more details.
