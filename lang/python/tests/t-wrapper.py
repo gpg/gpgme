e64bffe0307d14204b00a177a472cd4f99c07561
Justus Winter <justus@gnupg.org>
2016-05-12 15:27:50 +0200

    python: Add a test suite.
    * configure.ac: Add new Makefile.
    * lang/python/Makefile.am: Add subdirectory.
    * lang/python/tests/Makefile.am: New file.
    * lang/python/tests/t-wrapper.py: Likewise.
    
    Signed-off-by: Justus Winter <justus@gnupg.org>

---
+++ lang/python/tests/t-wrapper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2016 g10 Code GmbH
+#
+# This file is part of GPGME.
+#
+# GPGME is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# GPGME is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from pyme import core
+
+d0 = core.Data()
+assert d0.seek == d0.seek, "Generated wrapper functions are not cached"
