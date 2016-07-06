52efcf1ee9fc8ba4c6bd23d8fe4f5f7993ba9fb1
Justus Winter <justus@g10code.com>
2016-07-06 10:59:18 +0200

    python: Fix distcheck.
    * lang/python/INSTALL: Drop obsolete file.
    * lang/python/Makefile.am (EXTRA_DIST): Add missing files.
    (CLEANFILES): Remove generated files.
    (clean-local): Fix permissions of copied files.
    * lang/python/tests/Makefile.am (TESTS): Use our own setup and
    teardown scripts.
    (EXTRA_DIST): Add missing files.
    * lang/python/tests/final.py: New file.
    * lang/python/tests/initial.py: Likewise.
    
    Signed-off-by: Justus Winter <justus@g10code.com>

---
+++ lang/python/tests/initial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2016 g10 Code GmbH
+#
+# This file is part of GPGME.
+#
+# GPGME is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# GPGME is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import os
+import subprocess
+
+subprocess.check_call([os.path.join(os.getenv('top_srcdir'),
+                                    "tests", "start-stop-agent"), "--start"])
