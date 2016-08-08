b5e16b036f0045524a583d8a366d8a3366fc0005
Werner Koch <wk@gnupg.org>
2016-08-08 17:02:54 +0200

    core: Let GPGME_PROTOCOL_ASSUAN pass Assuan comments through.
    * src/engine-assuan.c (llass_new): Set ASSUAN_CONVEY_COMMENTS,
    --
    
    GnuPG-bug-id: 2429
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- src/engine-assuan.c
+++ src/engine-assuan.c
@@ -248,6 +248,7 @@ llass_new (void **engine, const char *file_name, const char *home_dir,
   if (err)
     goto leave;
   assuan_ctx_set_system_hooks (llass->assuan_ctx, &_gpgme_assuan_system_hooks);
+  assuan_set_flag (llass->assuan_ctx, ASSUAN_CONVEY_COMMENTS, 1);
 
   err = assuan_socket_connect (llass->assuan_ctx, file_name, 0, 0);
   if (err)
