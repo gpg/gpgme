7103315829847163f1c6f52cad25d1ddb33358f0
Andre Heinecke <aheinecke@intevation.de>
2017-01-02 13:11:10 +0100

    core: Always use runtime for gpgconf changes
    * src/engine-gpgconf.c (gpgconf_write): Add --runtime.
    * src/gpgme.h.in (gpgme_conf_opt_change): Document this
    behavior.
    
    --
    If a tool uses GPGME for changing configuration values it
    needs a way to ensure that these changes take effect. Otherwise
    users may change and see config values and do not understand
    why they are not working.

--- src/engine-gpgconf.c
+++ src/engine-gpgconf.c
@@ -708,7 +708,7 @@ gpgconf_write (void *engine, const char *arg1, char *arg2, gpgme_data_t conf)
 #define BUFLEN 1024
   char buf[BUFLEN];
   int buflen = 0;
-  char *argv[6];
+  char *argv[7];
   int argc = 0;
   int rp[2];
   struct spawn_fd_item_s cfd[] = { {-1, 0 /* STDIN_FILENO */}, {-1, -1} };
@@ -724,6 +724,7 @@ gpgconf_write (void *engine, const char *arg1, char *arg2, gpgme_data_t conf)
       argv[argc++] = gpgconf->home_dir;
     }
 
+  argv[argc++] = (char*)"--runtime";
   argv[argc++] = (char*)arg1;
   argv[argc++] = arg2;
   argv[argc] = NULL;
