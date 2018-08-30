gpg/gpgme

commit 08cd34afb762975b0273575035dacf69449ef241

core: Export gpgme_data_new_from_estream function.
Signed-off-by: Werner Koch <wk@gnupg.org>

src/gpgme.def
@@ -272,5 +272,7 @@ EXPORTS
     gpgme_op_encrypt_sign_ext             @202
     gpgme_op_encrypt_sign_ext_start       @203
 
+    gpgme_data_new_from_estream           @204
+
 ; END
 

src/libgpgme.vers
@@ -133,6 +133,9 @@ GPGME_1.1 {
 
     gpgme_op_decrypt_ext;
     gpgme_op_decrypt_ext_start;
+
+    gpgme_data_new_from_estream;
+
 };
 
 

commit 1d00fb987b903e245d484bddfe3c0a0aba670ac1

python bindings: estreams fix
* lang/python/src/core.py: Adjusted new_from_estream function to alias
  new_from_stream instead of fd.
* fixed the _gpgme import errors introduced in commit
  08cd34afb762975b0273575035dacf69449ef241 by changing the exported
  functions/types to match the inner module where all the work is
  done, rather than the outer one(s).

Tested-by: Ben McGinnes <ben@adversary.org>
Signed-off-by: Ben McGinnes <ben@adversary.org>

lang/python/src/core.py
@@ -1562,17 +1562,17 @@ class Data(GpgmeWrapper):
         self.wrapped = gpgme.gpgme_data_t_p_value(tmp)
         gpgme.delete_gpgme_data_t_p(tmp)
 
-    def new_from_estream(self, file):
-        """This wrap around gpgme_data_new_from_estream is an alias for
-        new_from_fd() method since in python there's no difference
-        between file stream and file descriptor"""
-        self.new_from_fd(file)
-
     def new_from_stream(self, file):
         """This wrap around gpgme_data_new_from_stream is an alias for
         new_from_fd() method since in python there's no difference
-        between file stream and file descriptor"""
+        between file stream and file descriptor."""
         self.new_from_fd(file)
+
+    def new_from_estream(self, file):
+        """This wrap around gpgme_data_new_from_estream is an alias for
+        new_from_fd() method since in python there's no difference
+        between file stream and file descriptor, but using fd broke."""
+        self.new_from_stream(file)
 
     def write(self, buffer):
         """Write buffer given as string or bytes.

src/gpgme.def
@@ -272,7 +272,7 @@ EXPORTS
     gpgme_op_encrypt_sign_ext             @202
     gpgme_op_encrypt_sign_ext_start       @203
 
-    gpgme_data_new_from_estream           @204
+    _gpgme_data_new_from_estream          @204
 
 ; END
 

src/libgpgme.vers
@@ -134,7 +134,7 @@ GPGME_1.1 {
     gpgme_op_decrypt_ext;
     gpgme_op_decrypt_ext_start;
 
-    gpgme_data_new_from_estream;
+    _gpgme_data_new_from_estream;
 
 };
 